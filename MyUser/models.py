5417577878a489cb3e32e80172a2d6f4e05dfa60
Abdelrahman Mohamed Mohamed <abdofawzi5@gmail.com>
2016-08-07 23:38:17 +0200

    add fk_locations to user

--- MyUser/models.py
+++ MyUser/models.py
@@ -1,5 +1,7 @@
+from django.utils.translation import  ugettext_lazy as _
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from Company.models import Location
 
 class User(AbstractUser):
-    date_of_birth = models.DateField(blank = True, null = True )
+    fk_locations = models.ManyToManyField(Location,verbose_name = _('access Location'),related_name = _('accessLocation'),help_text = _('Locations can access and edit'),blank = True, null = True)
