a916bc989820f077d58cb6e62420d94fdfb9d79e
Abdelrahman Mohamed <afawzi@abuerdan.com>
2016-08-19 01:53:26 +0200

    send dateFilter to views

--- StockDashboard/views.py
+++ StockDashboard/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
+from datetime import date
 
 @login_required(login_url='/admin/login/')
 def companyLevel(request):
     context = {}
+    dateFilter = request.GET.get('dateFilter')
+    print dateFilter
+    if dateFilter is None:
+        dateFilter = date.today()
+    print dateFilter
     return render_to_response('StockDashboard/companyLevel.html',context)
 
 
